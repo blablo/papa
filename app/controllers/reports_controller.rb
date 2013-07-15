c84295ca960440a9381d9773495e0481dd37be0a
blablo <blablo@gmail.com>
2013-07-15 12:18:30 -0500

    reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -3,7 +3,7 @@ class ReportsController < ApplicationController
   def index
     @cortes = Corte.order('created_at desc')
     @gastos = Gasto.no_corte_no_caja.order('created_at desc')
-    @saldo = Corte.sum(:total_venta) - Gasto.sum(:precio)
+    @saldo = @cortes.sum(:total_venta) - @gastos.sum(:precio) - @cortes.sum(:total_gastos)
   end
 
 end
